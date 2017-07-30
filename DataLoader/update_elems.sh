1ef845a226d7d0f4acb1090ec9c49de2806cf0f3
Vitor Rozsa <vitor.rozsa@hotmail.com>
2017-07-30 10:53:35 -0300

    Add missing copyright information

--- DataLoader/update_elems.sh
+++ DataLoader/update_elems.sh
@@ -1,3 +1,22 @@
+###
+ # Copyright © 2017 Vitor Rozsa, vitor.rozsa@hotmail.com
+ #
+ #	This file is part of Data Loader.
+ #	
+ #	Update Elements is free software: you can redistribute it and/or modify
+ #	it under the terms of the GNU General Public License as published by
+ #	the Free Software Foundation, either version 3 of the License, or
+ #	(at your option) any later version.
+ #
+ #	Update Elements is distributed in the hope that it will be useful,
+ #	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ #	GNU General Public License for more details.
+ #
+ #	You should have received a copy of the GNU General Public License
+ #	along with Update Elements. If not, see<http://www.gnu.org/licenses/>.
+###
+
 # For the folder where this script is located:
 #
 # Find each file with ".json" extension and create a list from its names.
--- GORef/GORef.cs
+++ GORef/GORef.cs
@@ -1,4 +1,21 @@
-﻿using System.Collections.Generic;
+﻿/**
+ * Copyright © 2017 Vitor Rozsa, vitor.rozsa@hotmail.com
+ *
+ *	Game Object Reference is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Game Object Reference is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with Game Object Reference. If not, see<http://www.gnu.org/licenses/>.
+ */
+
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
